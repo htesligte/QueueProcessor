6110c0a293db584239b58b34a37502559a98f831
Henk te Sligte <henk@comsi.nl>
2016-06-07 09:56:01 +0200

    threads weer terug naar 10

--- main.cpp
+++ main.cpp
@@ -17,13 +17,11 @@ int main()
   boost::thread socketThread{startSocketHandler, &workQueue};
 
   // thread pool for work - yeah I know I should have used boosts' threadpool :-(
-  /*for( int i = 0; i < 10; i++ )
+  for( int i = 0; i < 10; i++ )
   {
     WorkerThread *workerThread = new WorkerThread( &workQueue );
     new boost::thread( &WorkerThread::findWork, workerThread );
-  }*/
-  WorkerThread *workerThread = new WorkerThread( &workQueue );
-  workerThread->findWork();
+  }
 
   // hang main thread
   while(true) {
